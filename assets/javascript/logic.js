980e946c76e2ab64db90682c00bdbaf52f44e122
chris284891 <chris284891@gmail.com>
2019-07-22 17:00:48 -0400

    added javascript file and added an alert to submit button

---
+++ assets/javascript/logic.js
@@ -0,0 +1,9 @@
+$(".submit").click(function(){
+
+    Swal.fire({
+      type: 'success',
+      title: 'Thanks!',
+      text: 'Look forward to chatting with you soon!',
+      timer: 5000
+    })
+  });
