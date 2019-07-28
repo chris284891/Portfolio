e5eaa357e475b6c3d4c14381da8a3f8ea17a4350
chris284891 <chris284891@gmail.com>
2019-07-28 17:15:48 -0400

    changed telephone number to "tel" instead of "skype"

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,9 +1,9 @@
-$(".submit").click(function(){
+// $(".submit").click(function(){
 
-    Swal.fire({
-      type: 'Success!',
-      title: 'Thanks!',
-      text: 'I will be getting back to you shortly!',
-      timer: 5000
-    })
-  });
+//     Swal.fire({
+//       type: 'Success!',
+//       title: 'Thanks!',
+//       text: 'I will be getting back to you shortly!',
+//       timer: 5000
+//     })
+//   });
